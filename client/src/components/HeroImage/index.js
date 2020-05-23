b0dc60814b0c6c7e9f8c30ab762f76304f631945
gcmartin <gscalica@gmail.com>
2020-05-23 13:58:18 -0700

    Create HeroImage component and ShowUserEventContainer and add comments for some next steps

---
+++ client/src/components/HeroImage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HeroImage() {
+  return (
+    <img src="https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2552&q=80" alt="planner" />
+  )
+}
+
+export default HeroImage;
