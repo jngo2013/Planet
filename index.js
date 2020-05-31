7a6f2368e9c77cfca581811ee203f8b4a697f5e2
Jimmy Ngo <jngo2013@gmail.com>
2020-05-30 21:05:50 -0700

    connecting to Heroku

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 require('./services/passport');
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 // Connect database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/events', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://project03:password1@ds141320.mlab.com:41320/heroku_sr8c6jmt", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
 
 app.listen(PORT);
